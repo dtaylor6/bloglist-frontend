ec501b17743e3a031159e68ebbf2f648f3a27ca6
Daniel <dtaylor6@umbc.edu>
2023-01-29 19:14:19 -0500

    add like button test

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -39,14 +39,9 @@ test('<Blog> renders its URL and number of likes when is view is clicked', async
     }
   }
 
-  const mockHandler = jest.fn()
-
-  const { container } = render(<Blog blog={blog} toggleVisibility={mockHandler}/>)
+  const { container } = render(<Blog blog={blog} />)
   const div = container.querySelector('.blog')
   expect(div).toBeDefined()
-
-  expect(div).toHaveTextContent('test123')
-  expect(div).toHaveTextContent('John Doe')
 
   const user = userEvent.setup()
   const button = screen.getByText('view')
@@ -54,4 +49,32 @@ test('<Blog> renders its URL and number of likes when is view is clicked', async
 
   expect(div).toHaveTextContent('fake.com')
   expect(div).toHaveTextContent('10')
+})
+
+test('<Blog> renders its URL and number of likes when is view is clicked', async () => {
+  const blog = {
+    title: 'test123',
+    author: 'John Doe',
+    url: 'fake.com',
+    likes: 10,
+    user: {
+      name: 'Roto Rooter',
+      username: 'leetblogger123'
+    }
+  }
+
+  const likeMockHandler = jest.fn()
+
+  const { container } = render(<Blog blog={blog} updateBlog={likeMockHandler} />)
+  const div = container.querySelector('.blog')
+  expect(div).toBeDefined()
+
+  const user = userEvent.setup()
+  const viewButton = screen.getByText('view')
+  await user.click(viewButton)
+
+  const likeButton = screen.getByText('like')
+  await user.click(likeButton)
+  await user.click(likeButton)
+  expect(likeMockHandler.mock.calls).toHaveLength(2)
 })
